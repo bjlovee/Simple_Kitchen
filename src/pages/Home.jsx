0cc847d4c152573c8e7f85d82223adf38c9745aa
bjlovee <bj000051@yahoo.com>
2022-12-26 16:47:49 -0500

    setup react fronted /completed popular.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import Veggie from "../components/Veggie";
+import popular from "../components/Popular";
+
+
+
+
+ import React from 'react'
+import Popular from "../components/Popular";
+ 
+ function Home() {
+   return (
+     <div>
+        <Veggie />
+        <Popular />
+     </div>
+   )
+ }
+ 
+ export default Home
